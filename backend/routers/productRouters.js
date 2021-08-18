9549ae1912bcd1443491140d4af22c188b7896f1
TruongGiaBao <truonggiabao12345@gmail.com>
2021-08-18 22:07:44 +0700

    product Reviews

--- backend/routers/productRouters.js
+++ backend/routers/productRouters.js
@@ -4,10 +4,16 @@ const productRouter = express.Router();
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 productRouter.post('/seed',productControllers.seedProduct);
-productRouter.get('/', productControllers.getAllProducts);
-productRouter.get('/:id', productControllers.getProduct);
+
+productRouter.put('/review', isAuthenticatedUser, productControllers.createProductReviews);
+productRouter.delete('/review', isAuthenticatedUser, productControllers.deleteProductReview);
+productRouter.get('/review/:id', isAuthenticatedUser, productControllers.getProductReviews);
+
 productRouter.post('/admin/new', isAuthenticatedUser, authorizeRoles('admin'), productControllers.newProduct);
 productRouter.put('/admin/:id', isAuthenticatedUser, authorizeRoles('admin'), productControllers.updateProduct);
 productRouter.delete('/admin/:id', isAuthenticatedUser, authorizeRoles('admin'), productControllers.deleteProduct);
+
+productRouter.get('/', productControllers.getAllProducts);
+productRouter.get('/:id', productControllers.getProduct);
 
 module.exports = productRouter;
